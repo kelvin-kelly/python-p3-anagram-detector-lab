79cb84bf399919f9cf3ecb6b7027dd9032b48ada
Kelvin Machira <machirakelvin81@gmail.com>
2024-06-04 23:53:37 +0300

    Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+# your code goes here!# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted([letter for letter in word])
+
+    def match(self, arr):
+        matched = []
+        for i in arr:
+            if sorted([letter for letter in i]) == self.word:
+                matched.append(i)
+        return matched
+                
